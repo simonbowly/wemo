09d67d3e10dd50f49bb0027e7c41c4707e7e378d
Simon Bowly <simon.bowly@gmail.com>
2020-04-14 17:33:49 +1000

    Implemented common SOAP request generator. Added info/control CLI subcommands. Implemented GetInsightParams to get current power use and statistics.

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# WEMO API Methods
+
+Just a little control/query CLI so far:
+
+* Get current state, load, time-on  `python wemo.py info <device_ip> on`
+* Switch on `python wemo.py control <device_ip> on`
+* Switch off `python wemo.py control <device_ip> off`
+
+## Devices
+
+Testing this with a Wemo.Insight.D43 (unknown firmware version).
+
+## Sources
+
+* Ouimeaux library (https://github.com/iancmcc/ouimeaux), specifically:
+    * `ouimeaux/device/insight.py` which gives the field order for data returned in the GetInsightParams field
+    * `client.py` (contributed by BlackLight) which contains full SOAP requests for on/off switching (also use this to discover WEMO IP on local network if required)
+* Some insight API information from https://chameth.com/monitoring-power-with-wemo/
+* A partly correct list of services and fields from https://gist.github.com/nstarke/018cd98d862afe0a7cda17bc20f31a1e
--- wemo.py
+++ wemo.py
@@ -1,128 +1,141 @@
-#!/usr/bin/env python3
-#
-# Requirements:
-#     - `requests` package (`pip install requests`)
-#     - [Advised] static IP allocation for your WeMo devices
-
-# client.py from https://github.com/iancmcc/ouimeaux/
-#   :author: Fabio "BlackLight" Manganiello <info@fabiomanganiello.com>
-# insight API from https://chameth.com/monitoring-power-with-wemo/
-
-# Request to make
-#
-# POST /upnp/control/insight1 HTTP/1.1
-# Accept: */*
-# Accept-Encoding: gzip, deflate
-# Accept-Language: en-GB,en;q=0.8
-# Content-Type: text/xml
-# Origin: chrome-extension://aejoelaoggembcahagimdiliamlcdmfm
-# SOAPACTION: "urn:Belkin:service:insight:1#GetPower"
-# User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.29 Safari/537.36
-
-# <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-#     <s:Body>
-#         <u:GetPower xmlns:u="urn:Belkin:service:insight:1"/>
-#     </s:Body>
-# </s:Envelope>
-
-# Response
-#
-# <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-#     <s:Body>
-#         <u:GetPowerResponse xmlns:u="urn:Belkin:service:insight:1">
-#             <InstantPower>92170</InstantPower>
-#         </u:GetPowerResponse>
-#     </s:Body>
-# </s:Envelope>
-
-
 import re
 import textwrap
-
 from xml.dom.minidom import parseString
+from datetime import datetime
+from pprint import pprint
 
 import click
 import requests
 
 
-def post_basic_event(device: str, action: str, port: int = 49153, value=None):
-    state = action[3:]
-    value = value if value is not None else ""
-    response = requests.post(
-        f"http://{device}:{port}/upnp/control/basicevent1",
+GETINSIGHTPARAMS_FIELDS = [
+    "state",
+    "last_state_change",
+    "on_for_seconds",
+    "on_today_seconds",
+    "on_total_seconds",
+    "time_period",  # The period over which averages are calculated?
+    "unknown",
+    "current_power_mw",
+    "today_mw",
+    "total_mw",
+    "power_threshold_mw",
+]
+
+INSIGHT_STATES = {
+    0: "off",
+    1: "on",
+    8: "standby",
+}
+
+
+def parse_insight_data(data):
+    data = dict(zip(GETINSIGHTPARAMS_FIELDS, data.split("|")))
+    data = {
+        k: float(v)
+        if k == "total_mw"
+        else datetime.fromtimestamp(int(v))
+        if k == "last_state_change"
+        else INSIGHT_STATES[int(v)]
+        if k == "state"
+        else int(v)
+        for k, v in data.items()
+    }
+    return data
+
+
+def formulate_request(host, service, field, set_value=None, port=49153):
+    if set_value is None:
+        action = f"Get{field}"
+        value_tag = ""
+    else:
+        action = f"Set{field}"
+        value_tag = f"<{field}>{set_value}</{field}>"
+    return dict(
+        url=f"http://{host}:{port}/upnp/control/{service}1",
         headers={
             "User-Agent": "",
             "Accept": "",
             "Content-Type": 'text/xml; charset="utf-8"',
-            "SOAPACTION": f'"urn:Belkin:service:basicevent:1#{action}"',
+            "SOAPACTION": f'"urn:Belkin:service:{service}:1#{action}"',
         },
         data=re.sub(
             "\s+",
             " ",
             textwrap.dedent(
                 f"""
-            <?xml version="1.0" encoding="utf-8"?>
-            <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
-                    s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-                <s:Body>
-                    <u:{action} xmlns:u="urn:Belkin:service:basicevent:1">
-                        <{state}>{value}</{state}>
-                    </u:{action}
-                ></s:Body>
-            </s:Envelope>
-            """
+                <?xml version="1.0" encoding="utf-8"?>
+                <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+                        s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+                    <s:Body>
+                        <u:{action} xmlns:u="urn:Belkin:service:{service}:1">
+                            {value_tag}
+                        </u:{action}>
+                    </s:Body>
+                </s:Envelope>
+                """
             ),
         ),
     )
+
+
+def get_field(device_ip, service, field, **kwargs):
+    # TODO make aiohttp request variants
+    request = formulate_request(device_ip, service, field, **kwargs)
+    response = requests.post(**request)
     dom = parseString(response.text)
-    return dom.getElementsByTagName(state).item(0).firstChild.data
+    return dom.getElementsByTagName(field).item(0).firstChild.data
 
 
-def switch_on(device: str, **kwargs):
-    return post_basic_event(device=device, action="SetBinaryState", value=1, **kwargs)
+def set_field(device_ip, service, field, value, **kwargs):
+    request = formulate_request(device_ip, service, field, set_value=value, **kwargs)
+    response = requests.post(**request)
+    dom = parseString(response.text)
+    return dom.getElementsByTagName(field).item(0).firstChild.data
 
 
-def switch_off(device: str, **kwargs):
-    return post_basic_event(device=device, action="SetBinaryState", value=0, **kwargs)
+def get_insight_info(device_ip):
+    data = get_field(device_ip, service="insight", field="InsightParams")
+    return parse_insight_data(data)
 
 
-def is_switched_on(device: str, **kwargs):
-    return bool(int(post_basic_event(device=device, action="GetBinaryState", **kwargs)))
+def switch_on(device_ip: str):
+    set_field(device_ip, service="basicevent", field="BinaryState", value=1)
 
 
-def get_name(device: str, **kwargs):
-    return post_basic_event(device=device, action="GetFriendlyName", **kwargs)
+def switch_off(device_ip: str):
+    set_field(device_ip, service="basicevent", field="BinaryState", value=0)
 
 
-def get_device(device: str, **kwargs):
-    return {
-        "ip": device,
-        "name": get_name(device, **kwargs),
-        "switched_on": is_switched_on(device, **kwargs),
-    }
+def is_switched_on(device_ip: str):
+    return bool(int(get_field(device_ip, service="basicevent", field="BinaryState")))
 
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
 @click.argument("device-ip")
-@click.argument("command", type=click.Choice(["on", "off", "check"]))
-def cli(command, device_ip):
-    device = get_device(device_ip)
-    if command == "check":
-        click.echo(
-            f"WEMO '{device['name']}' is currently switched {'on' if device['switched_on'] else 'off'}"
-        )
-        return
-    if command == "on" and not device["switched_on"]:
-        click.echo(f"Switching on WEMO '{device['name']}'")
+def info(device_ip):
+    pprint(get_insight_info(device_ip))
+
+
+@cli.command()
+@click.argument("device-ip")
+@click.argument("command", type=click.Choice(["on", "off"]))
+def control(device_ip, command):
+    switched_on = is_switched_on(device_ip)
+    if command == "on" and not switched_on:
+        click.echo(f"Switching on wemo@{device_ip}")
         switch_on(device_ip)
         return
-    if command == "off" and device["switched_on"]:
-        click.echo(f"Switching off WEMO '{device['name']}'")
+    if command == "off" and switched_on:
+        click.echo(f"Switching off wemo@{device_ip}")
         switch_off(device_ip)
         return
-    click.echo(
-        f"WEMO '{device['name']}' is already switched {'on' if device['switched_on'] else 'off'}"
-    )
+    click.echo(f"wemo@{device_ip} is already switched {'on' if switched_on else 'off'}")
 
 
 if __name__ == "__main__":
